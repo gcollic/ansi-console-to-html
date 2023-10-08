ff67cb1e1254de468d802be61a7cdab21e3e77b9
Guillaume Collic <gcollic@gmail.com>
2023-10-08 22:11:24 +0200

    feat: add 256 colors table

--- tests/AnsiConsoleToHtml.DocAsTests/Sample.fs
+++ tests/AnsiConsoleToHtml.DocAsTests/Sample.fs
@@ -40,6 +40,55 @@ let verifyHtmlPart (name: string) (content: string) =
 let testHtmlPart name content =
     testTask name { do! verifyHtmlPart name content }
 
+let toCell (colors256: Color[]) i =
+    let isLightBackground =
+        match i with
+        | x when x < 16 -> x > 7
+        | x when x < 232 -> (x - 16) % 36 > 17
+        | x -> x > 243
+
+    let fontColor = if isLightBackground then "black" else "white"
+    $"<td style='background:{(colors256[i]).AsHexColor()};color:{fontColor}'>{i}</td>"
+
+let colorsToRow colors indexes =
+    indexes
+    |> List.map (toCell colors)
+    |> String.concat ""
+    |> fun s -> $"<tr>{s}</tr>\n"
+
+let colorsToTable colors indexes =
+    indexes
+    |> List.map (colorsToRow colors)
+    |> String.concat ""
+    |> fun s -> $"\n<table>\n{s}</table>\n"
+
+let doc16Colors colors =
+    [ [ 0..7 ]; [ 8..15 ] ]
+    |> colorsToTable colors
+    |> fun x ->
+        "\n<h2>0-15: 16 colors</h2>\nTODO depends on OS, etc. Standard and high intensity colors (4 bits)."
+        + x
+
+let doc216Colors colors =
+    [ for rowIndex in 0..5 -> [ for col in 0..35 -> (rowIndex * 36 + col + 16) ] ]
+    |> colorsToTable colors
+    |> fun s -> "\n<h2>16-231: 216 colors</h2>\n" + s
+
+let docGrayScaleColors colors =
+    [ [ 232..255 ] ]
+    |> colorsToTable colors
+    |> fun s -> "\n<h2>232-255: grayscale colors</h2>\n" + s
+
+let colors = Colors256.Table()
+
 [<Tests>]
 let tests =
-    testList "samples" [ testHtmlPart "Greeting world should work" <| Say.greetings "world" ]
+    testList
+        "samples"
+        [ testHtmlPart "Greeting world should work" <| Say.greetings "world"
+          testHtmlPart "ANSI colors"
+          <| ([ "<h1>256 colors table (8 bits)</h1>\n Cf. <a href='https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit'>Wikipedia</a>"
+                doc16Colors colors
+                doc216Colors colors
+                docGrayScaleColors colors ]
+              |> String.concat "\n") ]
