5cc73e0fd34317d21876be5d5eb0d4f0ffd6c4df
Guillaume Collic <gcollic@gmail.com>
2023-10-09 13:26:43 +0200

    chore: tests uses page name

--- tests/AnsiConsoleToHtml.DocAsTests/Sample.fs
+++ tests/AnsiConsoleToHtml.DocAsTests/Sample.fs
@@ -5,36 +5,15 @@ open AnsiConsoleToHtml
 
 open VerifyTests
 open VerifyExpecto
-open System.Text
-open System.Globalization
-open System.Text.RegularExpressions
 
 let verifySettings = VerifySettings()
 verifySettings.UseDirectory("expectations")
 verifySettings.DisableDiff()
 
-let removeDiacritics (text: string) =
-    let builder = new StringBuilder()
-
-    for c in text.Normalize(NormalizationForm.FormD) do
-        match CharUnicodeInfo.GetUnicodeCategory(c) with
-        | UnicodeCategory.NonSpacingMark -> ()
-        | _ -> builder.Append(c) |> ignore
-
-    builder.ToString().Normalize(NormalizationForm.FormC)
-
-let toSnakeCase (input: string) =
-    (Regex.Split(input, @"(?=[A-Z])|\W"))
-    |> Array.filter (fun x -> not (System.String.IsNullOrWhiteSpace(x)))
-    |> fun x -> System.String.Join("_", x)
-    |> fun x -> x.ToLowerInvariant()
-
-let toExpectationFileName (input: string) =
-    input |> removeDiacritics |> toSnakeCase
 
 let verifyHtmlPart (name: string) (content: string) =
     let settings = VerifySettings(verifySettings)
-    settings.UseFileName(toExpectationFileName name)
+    settings.UseFileName(name)
     Verifier.Verify("useless", content, "html", settings)
 
 let testHtmlPart name content =
@@ -85,8 +64,8 @@ let colors = Colors256.Table()
 let tests =
     testList
         "samples"
-        [ testHtmlPart "Greeting world should work" <| Say.greetings "world"
-          testHtmlPart "ANSI colors"
+        [ testHtmlPart "greeting" <| Say.greetings "world"
+          testHtmlPart "ansi_colors"
           <| ([ "<h1>256 colors table (8 bits)</h1>\n Cf. <a href='https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit'>Wikipedia</a>"
                 doc16Colors colors
                 doc216Colors colors
