81905aad6d95c07b18047d6fe6651a8ecc1e11a1
Guillaume Collic <gcollic@gmail.com>
2023-11-01 18:13:20 +0100

    chore: scaffold DocExporter

--- docAsTests/AnsiConsoleToHtml.DocExporter/Program.fs
+++ docAsTests/AnsiConsoleToHtml.DocExporter/Program.fs
@@ -1,11 +1,31 @@
 // For more information see https://aka.ms/fsharp-console-apps
 open System
 open System.Linq
-
-let showCommandLineArgs () =
-    Environment.GetCommandLineArgs()
-    |> Array.iteri (fun index arg -> printfn $"arg[{index}] = {arg}")
+open System.IO
 
 printfn "Hello World"
-showCommandLineArgs ()
+
+Environment.GetCommandLineArgs()
+|> Array.iteri (fun index arg -> printfn $"arg[{index}] = {arg}")
+
+let rootFolder = Directory.GetCurrentDirectory()
+
+Path.Combine(rootFolder, "docAsTests", "AnsiConsoleToHtml.DocExporter", "assets")
+|> Directory.GetFiles
+|> Array.iter (fun file ->
+    let fileName = Path.GetFileName(file)
+    let targetPath = Path.Combine(rootFolder, "doc", fileName)
+    File.Copy(file, targetPath, true)
+    printfn $"Copied '{file}'"
+    printfn $"    to '{targetPath}'")
+
+Path.Combine(rootFolder, "docAsTests", "AnsiConsoleToHtml.DocExporter", "templates")
+|> Directory.GetFiles
+|> Array.iter (fun file ->
+    let fileName = Path.GetFileName(file)
+    let targetPath = Path.Combine(rootFolder, "doc", fileName)
+    File.Copy(file, targetPath, true)
+    printfn $"Copied '{file}'"
+    printfn $"    to '{targetPath}'")
+
 exit 0
