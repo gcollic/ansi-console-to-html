7cd93489a9990feed9bece42fe26788ef382ae3d
Guillaume Collic <gcollic@gmail.com>
2023-11-03 12:53:44 +0100

    feat: doc export now cleans the folder before generation

--- docAsTests/AnsiConsoleToHtml.DocExporter/Program.fs
+++ docAsTests/AnsiConsoleToHtml.DocExporter/Program.fs
@@ -2,38 +2,54 @@
 open System
 open System.IO
 
-printfn "Hello World"
-
 Environment.GetCommandLineArgs()
 |> Array.iteri (fun index arg -> printfn $"arg[{index}] = {arg}")
 
 let rootFolder = Directory.GetCurrentDirectory()
+let toRelative s = Path.GetRelativePath(rootFolder, s)
+let docFolder = Path.Combine(rootFolder, "doc")
+
+docFolder
+|> Directory.GetFiles
+|> Array.filter (fun file -> not (file.EndsWith(".gitignore")))
+|> Array.iter (fun file ->
+    printfn $"Deleting '{toRelative file}'"
+    File.Delete file)
 
 Path.Combine(rootFolder, "docAsTests", "AnsiConsoleToHtml.DocExporter", "assets")
 |> Directory.GetFiles
 |> Array.iter (fun file ->
     let fileName = Path.GetFileName(file)
-    let targetPath = Path.Combine(rootFolder, "doc", fileName)
+    let targetPath = Path.Combine(docFolder, fileName)
     File.Copy(file, targetPath, true)
-    printfn $"Copied '{file}'"
-    printfn $"    to '{targetPath}'")
+    printfn $"Copied '{toRelative file}'"
+    printfn $"    to '{toRelative targetPath}'")
 
-Path.Combine(rootFolder, "docAsTests", "AnsiConsoleToHtml.DocExporter", "templates")
-|> Directory.GetFiles
+let mainLayout =
+    Path.Combine(
+        rootFolder,
+        "docAsTests",
+        "AnsiConsoleToHtml.DocExporter",
+        "templates",
+        "mainLayout.html"
+    )
+    |> File.ReadAllText
+
+[| mainLayout |]
 |> Array.iter (fun file ->
     let fileName = Path.GetFileName(file)
-    let targetPath = Path.Combine(rootFolder, "doc", fileName)
+    let targetPath = Path.Combine(docFolder, fileName)
     File.Copy(file, targetPath, true)
-    printfn $"Copied '{file}'"
-    printfn $"    to '{targetPath}'")
+    printfn $"Copied '{toRelative file}'"
+    printfn $"    to '{toRelative targetPath}'")
 
 Path.Combine(rootFolder, "docAsTests", "AnsiConsoleToHtml.DocAsTests", "expectations")
 |> Directory.GetFiles
 |> Array.iter (fun file ->
     let fileName = Path.GetFileName(file)
-    let targetPath = Path.Combine(rootFolder, "doc", fileName)
+    let targetPath = Path.Combine(docFolder, fileName)
     File.Copy(file, targetPath, true)
-    printfn $"Copied '{file}'"
-    printfn $"    to '{targetPath}'")
+    printfn $"Copied '{toRelative file}'"
+    printfn $"    to '{toRelative targetPath}'")
 
 exit 0
