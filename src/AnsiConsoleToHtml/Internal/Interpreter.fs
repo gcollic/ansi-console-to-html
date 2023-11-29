b5ecd6c68257ddea280a44495c396f4b6a1c5492
Guillaume Collic <gcollic@gmail.com>
2023-11-29 08:52:25 +0100

    refactor(Interpreter): readability

--- src/AnsiConsoleToHtml/Internal/Interpreter.fs
+++ src/AnsiConsoleToHtml/Internal/Interpreter.fs
@@ -4,62 +4,63 @@ open AnsiConsoleToHtml
 open AnsiModel
 open Parser
 
+type StyledText =
+    | Text of (string * AnsiStyle)
+    | NewLine
+
 let private (|Between|_|) first last code =
     if first <= code && code <= last then Some() else None
 
-let (|RGB|_|) (colors256: Color[]) codes =
-    match codes with
-    | [head] :: [2] :: [r] :: [g] :: [b] :: tail ->  Some (head, {R=uint8 r; G=uint8 g; B=uint8 b}, tail)
-    | [head] :: [5] :: [i] :: tail -> Some (head, colors256[i], tail)
-    | _ -> None
+let interpretCommands (colors256: Color[]) tokens =
 
-let rec private ansiCodesToStyle (colors256: Color[]) codes style =
-    match codes with
-    | RGB colors256 (38, color, tail) -> ansiCodesToStyle colors256 tail { style with Foreground = Some color }
-    | RGB colors256 (48, color, tail) -> ansiCodesToStyle colors256 tail { style with Background = Some color }
-    | (code :: options) :: tail ->
-        let newStyle =
+    let (|RGB|_|) codes =
+        match codes with
+        | [head]::[2]::[r]::[g]::[b]::tail ->  Some (head, {R=uint8 r; G=uint8 g; B=uint8 b}, tail)
+        | [head]::[5]::[i]::tail ->            Some (head, colors256[i]                     , tail)
+        | _ -> None
+
+    let rec applyCodesToStyle codes style =
+        match codes with
+        | RGB (38, color, tail) -> applyCodesToStyle tail { style with Foreground = Some color }
+        | RGB (48, color, tail) -> applyCodesToStyle tail { style with Background = Some color }
+        | (code :: options) :: tail ->
             match code with
-            | 0 -> AnsiStyle.Empty
-            | 1 -> { style with Bold = true }
-            | 3 -> { style with Italic = true }
+            | 0 ->                     AnsiStyle.Empty
+            | 1 ->                   { style with Bold = true }
+            | 3 ->                   { style with Italic = true }
             | 4 ->
                 match options with
                 | [] ->
                     match style.Underline with
                     | NoUnderline -> { style with Underline = StraightUnderline }
-                    | _ -> style
-                | [0] -> { style with Underline = NoUnderline }
-                | [1] -> { style with Underline = StraightUnderline }
-                | [2] -> { style with Underline = DoubleUnderline }
-                | [3] -> { style with Underline = CurlyUnderline }
-                | [4] -> { style with Underline = DottedUnderline }
-                | [5] -> { style with Underline = DashedUnderline }
-                | _ -> style
-            | 9 -> { style with Strikethrough = true }
-            | 21 -> { style with Underline = DoubleUnderline }
-            | 23 -> { style with Italic = false }
-            | 24 -> { style with Underline = NoUnderline }
-            | 29 -> { style with Strikethrough = false }
-            | Between 30 37 -> { style with Foreground = Some (colors256[code-30]) }
-            | 39 -> { style with Foreground = None }
-            | Between 40 47 -> { style with Background = Some (colors256[code-40]) }
-            | 49 -> { style with Background = None }
-            | Between 90 97 -> { style with Foreground = Some (colors256[code-82]) }
-            | Between 100 107 -> { style with Background = Some (colors256[code-92]) }
-            | _ -> style
-        ansiCodesToStyle colors256 tail newStyle
-    | _ -> style
+                    | _ ->             style
+                | [0] ->             { style with Underline = NoUnderline }
+                | [1] ->             { style with Underline = StraightUnderline }
+                | [2] ->             { style with Underline = DoubleUnderline }
+                | [3] ->             { style with Underline = CurlyUnderline }
+                | [4] ->             { style with Underline = DottedUnderline }
+                | [5] ->             { style with Underline = DashedUnderline }
+                |  _  ->               style
+            | 9  ->                  { style with Strikethrough = true }
+            | 21 ->                  { style with Underline = DoubleUnderline }
+            | 23 ->                  { style with Italic = false }
+            | 24 ->                  { style with Underline = NoUnderline }
+            | 29 ->                  { style with Strikethrough = false }
+            | Between 30 37   ->     { style with Foreground = Some colors256[code-30] }
+            | 39 ->                  { style with Foreground = None }
+            | Between 40 47   ->     { style with Background = Some colors256[code-40] }
+            | 49 ->                  { style with Background = None }
+            | Between 90 97   ->     { style with Foreground = Some colors256[code-82] }
+            | Between 100 107 ->     { style with Background = Some colors256[code-92] }
+            | _  ->                    style
+            |> applyCodesToStyle tail
+        | _ -> style
 
-type StyledText =
-    | Text of (string * AnsiStyle)
-    | NewLine
-
-let interpretCommands colors256 tokens =
-    let interpretToken (results, currentStyle) token =
+    let interpretToken (texts, currentStyle) token =
         match token with
-        | NonPrintable -> (results, currentStyle)
-        | RawNewLine -> (results @ [NewLine], currentStyle)
-        | RawText t -> (results @ [Text(t, currentStyle)], currentStyle)
-        | AnsiMCommand codes -> (results, ansiCodesToStyle colors256 codes currentStyle)
+        | NonPrintable -> (texts, currentStyle)
+        | RawNewLine -> (texts @ [NewLine], currentStyle)
+        | RawText t -> (texts @ [Text(t, currentStyle)], currentStyle)
+        | AnsiMCommand codes -> (texts, applyCodesToStyle codes currentStyle)
+
     List.fold interpretToken ([], AnsiStyle.Empty) tokens |> fst
