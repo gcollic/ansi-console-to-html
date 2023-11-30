b3cc30a5c3734604e4ecdb7d39863db9fe3ace10
Guillaume Collic <gcollic@gmail.com>
2023-11-30 08:58:30 +0100

    refactor(Renderer): nested functions

--- src/AnsiConsoleToHtml/Internal/Renderer.fs
+++ src/AnsiConsoleToHtml/Internal/Renderer.fs
@@ -37,58 +37,59 @@ let private toTextDecoration (style: AnsiStyle) =
         let decoration = String.concat " " x
         $"text-decoration:{decoration}")
 
-let private toHtmlStyle (style: AnsiStyle) =
-    let textDecoration = toTextDecoration style
-
-    seq {
-        if style.Foreground.IsSome then
-            yield $"color:{style.Foreground.Value.AsHexColor()}"
-
-        if style.Background.IsSome then
-            yield $"background:{style.Background.Value.AsHexColor()}"
-
-        if style.Bold then
-            yield "font-weight:900"
-
-        if style.Italic then
-            yield "font-style:italic"
-
-        if textDecoration.IsSome then
-            yield textDecoration.Value
-
-    }
-    |> mapIfNotEmpty (String.concat ";")
-
-let private toSpan ansiStyle innerContent =
-    match toHtmlStyle ansiStyle with
-    | None -> innerContent
-    | Some style -> $"<span style='{style}'>{innerContent}</span>"
-
-let rec private convertToHtmlParts tokens =
-    match tokens with
-    | NewLine :: tail -> "\n" :: convertToHtmlParts tail
-    | Text(text, style) :: tail ->
-        let escapedText = HttpUtility.HtmlEncode text
-
-        let span =
-            match style with
-            // to have different color for strikethrough and underline, you need to nest two spans
-            | { Strikethrough = false }
-            | { Underline = NoUnderline }
-            | { UnderlineColor = None } -> toSpan style escapedText
-            | _ ->
-                escapedText
-                |> toSpan {
-                    AnsiStyle.Empty with
-                        Underline = style.Underline
-                        UnderlineColor = style.UnderlineColor
-                }
-                |> toSpan { style with Underline = NoUnderline }
-
-        span :: convertToHtmlParts tail
-    | [] -> []
-
 let convertStyledTextToHtml (colors256: Color[]) tokens =
+
+    let toHtmlStyle (style: AnsiStyle) =
+        let textDecoration = toTextDecoration style
+
+        seq {
+            if style.Foreground.IsSome then
+                yield $"color:{style.Foreground.Value.AsHexColor()}"
+
+            if style.Background.IsSome then
+                yield $"background:{style.Background.Value.AsHexColor()}"
+
+            if style.Bold then
+                yield "font-weight:900"
+
+            if style.Italic then
+                yield "font-style:italic"
+
+            if textDecoration.IsSome then
+                yield textDecoration.Value
+
+        }
+        |> mapIfNotEmpty (String.concat ";")
+
+    let toSpan ansiStyle innerContent =
+        match toHtmlStyle ansiStyle with
+        | None -> innerContent
+        | Some style -> $"<span style='{style}'>{innerContent}</span>"
+
+    let rec convertToHtmlParts tokens =
+        match tokens with
+        | NewLine :: tail -> "\n" :: convertToHtmlParts tail
+        | Text(text, style) :: tail ->
+            let escapedText = HttpUtility.HtmlEncode text
+
+            let span =
+                match style with
+                // to have different color for strikethrough and underline, you need to nest two spans
+                | { Strikethrough = false }
+                | { Underline = NoUnderline }
+                | { UnderlineColor = None } -> toSpan style escapedText
+                | _ ->
+                    escapedText
+                    |> toSpan {
+                        AnsiStyle.Empty with
+                            Underline = style.Underline
+                            UnderlineColor = style.UnderlineColor
+                    }
+                    |> toSpan { style with Underline = NoUnderline }
+
+            span :: convertToHtmlParts tail
+        | [] -> []
+
     convertToHtmlParts tokens
     |> String.Concat
     |> fun str ->
